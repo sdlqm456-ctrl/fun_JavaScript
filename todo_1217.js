0c3225cae87aebeb001516d7a7ecf6405052f7d8
sdlqm456-ctrl <sdlqm456@gmail.com>
2025-12-17 23:19:28 +0900

    Add files via upload
    재수정본 업로드

---
+++ todo_1217.js
@@ -0,0 +1,9 @@
+// 이름입력
+let name = prompt ("이름을 입력하세요");
+console.log('이름은'  + name + '입니다');
+//연락처 입력
+let phonenumder = prompt ("전화번호를 입력하세요");
+console.log('전화번호는' + phonenumder + '입니다');
+//주소 입력 
+let Address = prompt ("주소를 입력하세요")
+console.log('주소는' + Address + '입니다');
