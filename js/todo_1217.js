6bb67936ad7536b274858143263e6184041f406e
장수연 <sdlqm456@email.com>
2025-12-18 09:55:56 +0900

    branch 연습

--- js/todo_1217.js
+++ js/todo_1217.js
@@ -5,3 +5,13 @@
 // 콘솔에 "이름은 000", 전화번호는 010-0000-000, 주소는 대구광역시 00구 00동 000번지
 // git commit -m "12/17 숙제완료"
 //git push -u origin main
+
+// 이름입력
+let name = prompt ("이름을 입력하세요");
+console.log('이름은'  + name + '입니다');
+//연락처 입력
+let phonenumder = prompt ("전화번호를 입력하세요");
+console.log('전화번호는' + phonenumder + '입니다');
+//주소 입력 
+let Address = prompt ("주소를 입력하세요")
+console.log('주소는' + Address + '입니다');
