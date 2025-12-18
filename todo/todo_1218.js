493c4fa100008368be35ba11d70abeb06edb4310
장수연 <sdlqm456@email.com>
2025-12-18 17:47:22 +0900

    12월 18일 수업완료

---
+++ todo/todo_1218.js
@@ -0,0 +1,11 @@
+// todo_1218.js
+// Math.rendom() 활용31 ~ 100까지 생성
+// 5번 실행 (for 명령어 활용)
+// 합계, 평균, 최대값 => "합계는 480,  평균 96, 최대 99"
+//  함수 : scoreInfo => scoreInfo()
+
+// 추가로 더 하고싶으면 & 몇번 실행 (10 매개값으로 받아서 10번 실행)
+// scoreInfo(10)
+// git add .
+// git commit -m "18일 숙제완료"
+// git push origin main
--- todo_1217.js
+++ todo_1217.js
@@ -1,11 +1,3 @@
-// 15-2, 15-3 장을 정독 (책: 486p ~ 493p)
-// git 설치
-// Dev/fun_javascript = git clone git 주소
-// 이름, 연락처, 주소 정보를 입력하면 
-// 콘솔에 "이름은 000", 전화번호는 010-0000-000, 주소는 대구광역시 00구 00동 000번지
-// git commit -m "12/17 숙제완료"
-//git push -u origin main
-
 // 이름입력
 let name = prompt ("이름을 입력하세요");
 console.log('이름은'  + name + '입니다');
