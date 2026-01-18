ee2856de835fb9a3a3fcbccdb655ca46feb23684
sdlqm456-ctrl <sdlqm456@gmail.com>
2026-01-18 18:52:13 +0900

    Update function1.js

--- calss_1/js/function1.js
+++ calss_1/js/function1.js
@@ -1,43 +1,32 @@
 // function1.js
-// 값 /기능
+
 //num = 0;
-// doIt = function() {
-//         기능실행
-//}
+// doIt = function() { 기능실행}
 
-// 두 수를 비교해서 큰 수를 출력가능
+// 두 수를 비교해서 큰 수를 출력
 // 함수 선언
-function showMax(a, b) {
-  //paramater (매개변수)
-  if (a > b) {
-    console.log(`큰 값은 ${a}`);
+function showMax(A, B) {
+  // 매개변수: 함수로 들어오는 값
+  if (A > B) {
+    console.log(`큰 값은 ${A}`);
   } else {
-    console.log(`큰 값은 ${b}`);
+    console.log(`큰 값은 ${B}`);
   }
-} // end of showMax(a,b) 
-
-let n1 = 10,
-  n2 = 20;
-// 함수 호출
-showMax(n1, n2); // arguement 매개값
-
-let m1 = 30,
-  m2 = 40;
-showMax(n1, n2); // arguement 매개값
+}
 
 if (n1 > n2) {
-  console.log(`큰값은 ${n1}`);
+  console.log(`큰 값은 ${n1}`);
 } else {
-  console.log(`큰값은 ${n2}`);
+  console.log(`큰 값은  ${n2}`);
 }
 
-// 매개변수 2개, 2개의 곱을 콘솔에 출력하는 함수 getMulti 함수
-
-function getMulti(a,b) {
+// 매개변수가 2개일때 2개의 곱을 콘솔에 출력
+// getMulti: 여러개의 데이터를 한번에 가져오기
+function getMulti(a, b) {
   let result = a * b;
-  console.log (`두 수의 곱은 ${a}`);
-  return result; // 반환
+  console.log(`두 수의 곱은 ${result}`);
+  return result; // 반환 (위의 계산결과를 밖으로 전달)
 }
 let result = getMulti(m1, 50);
-result = getMulti(2,3);
-console.log(result);
+result = getMulti(2, 3);
+console.log(result); // 최종결과 출력
