fbc215725569081e67ca244babea06ea9a978dc7
sdlqm456-ctrl <sdlqm456@gmail.com>
2026-01-20 23:14:00 +0900

    Update function3.js

--- calss_1/js/function3.js
+++ calss_1/js/function3.js
@@ -1,22 +1,28 @@
 // function3.js
+
 // 함수표현식
-const sum = function (a,b) {
+const sum = function (a, b) {
   return a + b;
 };
-console.log(sum);
-let result = sum(2,3);
 
-const plus = sum;
-result = plus(3,4);
-  
-// 즉시실행 함수
-resuit = (function (x,y) {
+console.log(sum);
+let result = sum(2, 3);
+
+// 함수를 다른 변수에 담아서 호출
+// 함수를 값처럼 쓰기위해 변수에 담아서 표현
+const plus = sum; // 함수 실행결과를 저장하는 것
+result = plus(3, 4);
+
+// 즉시실행 함수(함수를 만들고 바로 실행)
+// 여기서 function => 함수 표현식
+result = (function (x, y) {
+  // 3항 연산식 (x가 y보다 크면 x 또는 y 출력)
   return x > y ? x : y;
-}) (45,29);
-console.log (result);
+})(45, 29);
+console.log(result);
 
 // 화살표 함수. (매개변수) => { 기능구현 }
 const hi = (name) => {
   return `hello ! ${name}`;
 };
-console.log(hi('김민수'));
+console.log(hi("김민수"));
