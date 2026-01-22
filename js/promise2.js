bbaa5333f96754aac89110e3d6171e6f85e07da9
sdlqm456-ctrl <sdlqm456@gmail.com>
2026-01-22 23:24:56 +0900

    Update promise2.js

--- js/promise2.js
+++ js/promise2.js
@@ -1,28 +1,27 @@
-// promise2.js
-// resolve: 성공, reject: 실패
-// promise => 비동기 처리 방식을 동기처리로 처리되게 만들어주는 과정
+// promise2. js
+
+// promise: 비동기 처리 방식 => 동기처리방식
+// resolve: 성공 / reject: 실패
 function orderMenu(meun) {
   return new Promise((resolve, reject) => {
     console.log(`${meun}을 주문 받았습니다`);
-
+    // setTimeout: 특정시간이 지난 다음 코드를 실행 (타이머 설정)
     setTimeout(() => {
       if (meun == "라면") {
-        resolve("라면 조리완료 했습니다");
+        resolve("라면 조리 완료");
       } else {
         reject("품절 입니다");
       }
     }, 1500);
   });
 }
-
 function delivery() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve("전달완료");
+      resolve("배달 완료");
     }, 2000);
   });
 }
-
 // orderMenu("라면") // 반환해주는 값이 promise 객체
 //   .then((data) => {
 //     console.log(data); // 로그출력
@@ -35,9 +34,10 @@ function delivery() {
 //     console.error(err); // 에러출력
 //   });
 
-// asyns, await (비동기 처리방식 => 동기처리 방식으로 처리)
+// async, await: 비동기 처리 방식 => 동기처리방식
 async function orderFood() {
-  await orderMenu("라면");
+  // async: 함수 앞에 붙이는 키워드 (비동기,Promise를 반환)
+  await orderMenu("라면"); // await: 프라미스가 처리될 때 까지 함수실행을 기다림
   let result = await delivery();
   console.log(result);
 }
